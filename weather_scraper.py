2cf37a3cd35176e4d11c8d4e7289f50d52ecb014
gjmessino <gabyjmessino@gmail.com>
2026-05-26 15:05:22 -0700

    fiddled around with what to do

--- weather_scraper.py
+++ weather_scraper.py
@@ -1,5 +1,4 @@
 from time import sleep
-import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -14,10 +13,7 @@ try:
 except Exception as e:
     print(f"An exception occurred: {type(e).__name__} {e}")
 
-world_row = driver.get_elements(By.XPATH, '/html/body/div[5]/section[1]/div/section/div[1]/div/table/tbody/tr[1]')
-for items in world_row:
-    cities = items.get_elements(By.CSS_SELECTOR, 'a')
-    print(cities.text)
-
+nav_bar = driver.find_element(By.CSS_SELECTOR, 'div.my-city__item')
+local_url = nav_bar.get_attribute('href')
 
 driver.quit()
