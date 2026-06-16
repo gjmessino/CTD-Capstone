e279fe2df157d57c01e89a0574b48a550bbe7d4b
Gaby <145401032+gjmessino@users.noreply.github.com>
2026-06-16 13:00:52 -0700

    installed streamlit and started app

---
+++ programs/streamlit_app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+# st.title("My First Streamlit App")  # Adds a big title at the top of the app
+# st.header("Section 1")  # Adds a section header — good for breaking content into parts
+# st.subheader("Header")  # Slightly smaller than header — useful for structure
+# st.subheader("Subheader")  # Another level down — keeps things organized
+# st.text("Simple text")  # Displays plain, unformatted text — like a basic message
+# st.markdown("**Bold** and *italic* text")  # Markdown lets you add simple formatting like bold and italics
+
+st.title("International Weather Report")
+st.header("Find the Weather in Your City")
