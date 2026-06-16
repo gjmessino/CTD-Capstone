78d5d1a5bb506820567f827e6072b53538e8dc02
Gaby <145401032+gjmessino@users.noreply.github.com>
2026-06-16 15:53:34 -0700

    added streamlit features

--- programs/streamlit_app.py
+++ programs/streamlit_app.py
@@ -1,14 +1,14 @@
 import streamlit as st
-import sqlite3
 import pydeck as pdk
 import pandas as pd
+import plotly.express as px
 
 # Retrieve Data
-with  sqlite3.connect("./weather_data.db") as conn:
-    capitals = conn.query("SELECT * FROM Capitals__215;")
-    most_popular = conn.query("SELECT * FROM Most_Popular__143;")
-    least_popular = conn.query("SELECT * FROM Least_Popular__472;")
-weather_data = st.connection("./db/weather_data.db", type="sql")
+conn = st.connection("./weather_data.db","sql")
+capitals = conn.query("SELECT * FROM Capitals__215;", ttl=600)
+most_popular = conn.query("SELECT * FROM Most_Popular__143;", ttl=600)
+least_popular = conn.query("SELECT * FROM Least_Popular__472;", ttl=600)
+world_data = pd.concat([capitals, most_popular, least_popular]).drop_duplicates().reset_index(drop=True)
 
 #Streamlit Set Up
 st.title("International Weather Report")
@@ -25,31 +25,31 @@ COLOR_BREWER_BLUE_SCALE = [
     [67, 162, 202],
     [8, 104, 172],
 ]
-
-popular_map = pdk.Layer(
+layer = pdk.Layer(
     "HeatMapLayer",
-    data = most_popular,
-    get_position = ["longitude", "latitude"],
-    color_range = COLOR_BREWER_BLUE_SCALE,
-    get_weight = "temperature"
-)
-capitals_map = pdk.Layer(
-    "HeatMapLayer",
-    data = capitals,
-    get_position =["longitude", "latitude"],
-    color_range = COLOR_BREWER_BLUE_SCALE,
-    get_weight = "temperature"
-)
-least_pop_map = pdk.Layer(
-    "HeatMapLayer",
-    data = least_popular,
+    data = world_data,
     get_position = ["longitude", "latitude"],
     color_range = COLOR_BREWER_BLUE_SCALE,
     get_weight = "temperature"
 )
 r = pdk.Deck(
-    layers=[popular_map,capitals_map,least_pop_map],
+    layers=layer,
     initial_view_state=pdk.data_utils.compute_view(capitals[['Longitude','Latitude']]),
     map_provider="mapbox",
 )
 r.to_html("heatmap_layer.html")
+
+#Charts
+fig1 = px.histogram(world_data,
+                   x = "temperatureF",
+                   title = "Most Common Heat World Wide (Farenheit)")
+fig1.write_html('common_heat.html')
+st.plotly_chart(fig1)
+
+fig2 = px.histogram(world_data,
+                   x = "temperatureC",
+                   title = "Most Common Heat World Wide (Celsius)")
+fig1.write_html('common_heat.html')
+st.plotly_chart(fig1)
+
+st.dataframe(world_data)
